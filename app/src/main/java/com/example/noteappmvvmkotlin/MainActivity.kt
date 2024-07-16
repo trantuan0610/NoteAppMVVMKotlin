0b14bcbf64cacfc1a49d3c09879271b047f063da
tuantran <tuantd@miagi-so.net>
2024-07-16 16:46:32 +0700

    fix back button

--- app/src/main/java/com/example/noteappmvvmkotlin/MainActivity.kt
+++ app/src/main/java/com/example/noteappmvvmkotlin/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.noteappmvvmkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
-import androidx.navigation.NavController
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var navController : NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-          navController = findNavController(R.id.fragmentContainerView)
 
-        setupActionBarWithNavController(navController)
+        val actionBar: ActionBar? = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowHomeEnabled(true)
+        }
+
     }
 
-    override fun onNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onNavigateUp()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
